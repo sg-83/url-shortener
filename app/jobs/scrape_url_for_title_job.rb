676ee41efecf26b4a8485425e51bc5eb76ce9481
sg_83 <shirajg@protonmail.com>
2018-11-11 20:36:48 -0500

    Track parsing and network errors using the status field. Set up the Enum for this field to keep things neat and readable.

--- app/jobs/scrape_url_for_title_job.rb
+++ app/jobs/scrape_url_for_title_job.rb
@@ -8,15 +8,20 @@ class ScrapeUrlForTitleJob < ActiveJob::Base
       scrape = HTTParty.get(url.full)
     rescue
       logger.error('Scraping Failed')
+      # If parsing the http request fails, let's set a status so
+      # we know what went wrong. We can try to fix it later
+      url.status = Url.statuses['network_error']
       return
     end
 
     if scrape.success?
       title = Nokogiri::HTML(scrape.body).css('title').text
       url.title = title
+      url.status = Url.statuses['ok']
       url.save
       logger.info("Ran Job, parsed title as #{title}")
     else
+      url.status = Url.statuses['parse_error']
       logger.error('Parsing Failed')
     end
   end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,10 @@
 class Url < ActiveRecord::Base
+  enum status: {
+    ok: 0,
+    parse_error: 1,
+    network_error: 2
+  }
+
   def shorten!
     self.shortened = IdConverter.encode(self.id)
     self.save
